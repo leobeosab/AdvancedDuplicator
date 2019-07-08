ad45f4e74a6192ac1957c09306179e85cd205848
Ryan Wise <ryan@ryanwise.me>
2019-07-07 22:39:18 -0400

    initial commit

---
+++ Assets/Editor/AdvancedDuplicator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+public class AdvancedDuplicator : EditorWindow
+{
+
+    private GameObject selectedObject;
+
+    private DynamicVector3Input objPosition;
+    private DynamicVector3Input objRotation;
+    private DynamicVector3Input objScale;
+
+    private Transform transform;
+
+    [MenuItem("Window/Utilities/AdvancedDuplicator")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(AdvancedDuplicator));
+        
+    }
+
+    private void OnSelectionChange()
+    {
+        SetItemValues();
+        Repaint();
+    }
+
+    private void SetItemValues()
+    {
+        this.selectedObject = Selection.activeGameObject;
+    }
+
+    private void initializeObjects()
+    {
+        this.objPosition = new DynamicVector3Input("Position");
+        this.objRotation = new DynamicVector3Input("Rotation");
+        this.objScale = new DynamicVector3Input("Scale");
+    }
+
+    private void SetValuesWithActiveObject()
+    {
+        this.objPosition.vectorSet = this.selectedObject.transform.position;
+        this.objRotation.vectorSet = this.selectedObject.transform.rotation.eulerAngles;
+        this.objScale.vectorSet = this.selectedObject.transform.localScale;
+    }
+
+    private void duplicate()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        string name = this.selectedObject == null || Selection.gameObjects.Length > 1 ? "No object selected" : this.selectedObject.name;
+
+        GUILayout.Label("Duplicate Object: " + name, EditorStyles.boldLabel);
+        if (this.selectedObject == null || Selection.gameObjects.Length > 1)
+            return;
+        if (this.objPosition == null)
+        {
+            initializeObjects();
+            SetItemValues();
+        }
+
+        GUILayout.Label("Offsets: ");
+        this.objPosition.drawGUI();
+        this.objRotation.drawGUI();
+        this.objScale.drawGUI();
+
+        if (GUILayout.Button("Duplicate"))
+            duplicate();
+    }
+
+}
---
+++ Assets/Editor/DynamicVector3Input.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class DynamicVector3Input
+{
+    // Name of input
+    private string name;
+
+    private string x;
+    private string y;
+    private string z;
+
+    public Vector3 vectorSet 
+    {
+        set
+        {
+            this.x = value.x.ToString();
+            this.y = value.y.ToString();
+            this.z = value.z.ToString();
+        }
+    }
+
+ 
+    public DynamicVector3Input(string name)
+    {
+        this.name = name;
+
+        this.x = "";
+        this.y = "";
+        this.z = "";
+    }
+
+    public Rect drawGUI()
+    {
+        Rect r = EditorGUILayout.BeginHorizontal();
+        GUILayout.Label(this.name, 
+            GUILayout.MaxWidth(180),
+            GUILayout.MinWidth(80));
+        GUILayout.FlexibleSpace();
+        GUILayout.FlexibleSpace();
+
+        GUILayout.Label("X");
+        EditorGUILayout.TextField(x);
+        GUILayout.Label("Y");
+        EditorGUILayout.TextField(y);
+        GUILayout.Label("Z");
+        EditorGUILayout.TextField(z);
+        EditorGUILayout.EndHorizontal();
+        return r;
+    }
+
+    public Vector3 getVector3OffsetAtPoint(int n, Vector3 input)
+    {
+        float xVal = float.Parse(this.x);
+        float yVal = float.Parse(this.y);
+        float zVal = float.Parse(this.z);
+
+        return new Vector3(xVal + input.x * n, yVal + input.y * n, zVal + input.z * n);
+    }
+}
